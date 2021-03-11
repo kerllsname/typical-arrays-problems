4546e14d1e01399ad2c251a07df984edc2793af5
kerllsname <kirillpotapovnow@gmail.com>
2021-03-11 18:23:25 +0300

    feat: make a task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if(array === undefined || array.length === 0) return 0;
+  for(let i = 0; i <= array.length; i++){
+    if (array[i] < min) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if(array === undefined || array.length === 0) return 0;
+  array.forEach(function(el){
+    if (el > max) max = el; 
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let temp = 0;
+  if(array === undefined || array.length === 0) return 0;
+  for(let i = 0; i <= array.length - 1; i++){
+    temp += array[i];
+    console.log(temp)
+  }
+  let avg = temp / (array.length);
+  return avg;
 }
